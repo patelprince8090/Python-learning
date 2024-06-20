02635f48f160bd1f1f370aae5b1710b3aabea0d3
patelprince8090 <prince019081912j@gmail.com>
2024-06-20 07:54:14 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,13 @@
+a=12
+b="prince"
+c=12.156
+d=("prince",12,13,14)
+e=["prince",14,456,1]
+f={12,13,15,197,9,46,752148963}
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
