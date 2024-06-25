ed6766b2e778d1bd88050bd621462c9ccae7f9e8
patelprince8090 <prince019081912j@gmail.com>
2024-06-25 10:09:13 +0530

    Add files via upload

---
+++ prince   patel.py
@@ -0,0 +1,17 @@
+a=34
+b="prince"
+print(a,b)
+print(type(a))
+print(type(b))
+
+a=96
+b=0.64
+c="pransu"
+d=4>5
+print(a,b)
+print(b,c)
+print(c,d)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
