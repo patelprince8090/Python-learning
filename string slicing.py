cf00cb6c34bab5a5d65909bb74b29d843b10fae9
patelprince8090 <prince019081912j@gmail.com>
2024-06-26 08:49:51 +0530

    Add files via upload

---
+++ string slicing.py
@@ -0,0 +1,11 @@
+greeting = "GOOD MORNING, "
+name = "prince"
+print(greeting+name)
+
+greeting = "good afternoon, "
+name = "prince"
+print(greeting + name )
+
+greeting = "goog night, "
+name = "prince"
+print(greeting + name)
