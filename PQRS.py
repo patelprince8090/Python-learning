97c7fa989bf6398bd3c1260e142f658767a6ee17
patelprince8090 <prince019081912j@gmail.com>
2024-06-27 10:05:52 +0530

    Add files via upload

---
+++ PQRS.py
@@ -0,0 +1,14 @@
+a=int(input("Enter a number: "))
+b=int(input("Enter a number: "))
+
+
+sum=a+b
+difference=a-b
+product=a*b
+divident=a/b
+
+
+print(sum)
+print(difference)
+print(product)
+print(divident)
