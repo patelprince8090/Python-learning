4ca304d9ea25958c7bdfb4781aa370339b59c7cc
patelprince8090 <prince019081912j@gmail.com>
2024-07-05 10:08:29 +0530

    Add files via upload

---
+++ string functions.py
@@ -0,0 +1,12 @@
+story = "After a long day at work, his legs ached a lot. He took off his traffic constable uniform first, and then his leg braces"
+
+# len function
+print(len(story))
+
+# count function
+print(story.count("l"))
+print(story.count("a"))
+
+# endswith function
+print(story.endswith("leg"))
+print(story.endswith("braces"))
