5f4a096513219fc22855a050ab154acc6e00282f
patelprince8090 <prince019081912j@gmail.com>
2024-07-06 08:39:57 +0530

    Add files via upload

---
+++ string functions 1.py
@@ -0,0 +1,22 @@
+story = "After a long day at work his legs ached a lot He took off his traffic constable uniform first and then his leg braces"
+
+# len function
+# print(len(story))
+
+# count function
+# print(story.count("l"))
+# print(story.count("a"))
+
+# endswith function 
+# print(story.endswith("leg"))
+# print(story.endswith("braces"))
+
+# capitalize function
+# print(story.capitalize())
+
+# # find function
+# print(story.find("after"))
+# print(story.find("his"))
+
+# replace function
+# print(story.replace("after" , "codewithprince"))
