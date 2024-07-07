1f6c1416d58ff268a254bfa4fa8a1465409e5218
patelprince8090 <prince019081912j@gmail.com>
2024-07-07 10:12:46 +0530

    Add files via upload

---
+++ escape sequences.py
@@ -0,0 +1,11 @@
+story = "Prince is good. \nhe is very good"
+print(story)
+
+story = "Prince is good. \the is very good"
+print(story)
+
+story = "Prince is good. he is ve\\ry good"
+print(story)
+
+story = "Prince is good. \nhe\tis ve\\ry good"
+print(story)
