c0ca779b048a44c7939d25e8f1095cf237ccb9f5
patelprince8090 <prince019081912j@gmail.com>
2024-07-10 08:53:04 +0530

    Add files via upload

---
+++ list method 1.py
@@ -0,0 +1,18 @@
+# l1 = [1,2,3,35,15,24,26,57,59]
+# l1.append(55)
+# l1.sort()
+# l1.reverse()
+# l1.insert(1,100)
+# l1.pop(2)
+# l1.remove(57)
+# print(l1)
+
+l2 = [1,2,3,4,5,6,7,8,9]
+l2.append(10)
+l2.append(11)
+l2.sort()
+l2.reverse()
+l2.insert(1,124)
+l2.pop(2)
+l2.remove(9)
+print(l2)
