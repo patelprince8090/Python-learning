6a804ba7dc61a2d83fec58b4c159353b7604d5eb
patelprince8090 <prince019081912j@gmail.com>
2024-07-11 11:27:33 +0530

    Add files via upload

---
+++ dictionary syntax.py
@@ -0,0 +1,12 @@
+mydict = {
+         "fast": "In a quick respect",
+         "prince": "A coder",
+         "marks":[25,20,16],
+         "otherdict": {'prince'}
+          }
+
+# print(mydict['fast'])
+# print(mydict['prince'])
+mydict['marks'] = [21,22]
+print(mydict['marks'])
+print(mydict['otherdict']['prince'])
