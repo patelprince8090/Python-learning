7c8687bf340a630c5c9c1d865ebb9e98bfe8c4f3
patelprince8090 <prince019081912j@gmail.com>
2024-07-14 10:37:33 +0530

    Add files via upload

---
+++ conditionals.py
@@ -0,0 +1,9 @@
+a = 64
+if(a>21):
+    print("The velue of a is greater than 21")
+elif(a>60):
+    print("The velue of a is greater than 60")
+elif(a>51):
+    print("The velue of a is greater than 51")
+else:
+    print("The velue of a is greater than 21 or 51")
