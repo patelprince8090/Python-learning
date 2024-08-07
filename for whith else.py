e164a8267c3f6afcbafa2cb0653211f678fc0707
patelprince8090 <prince019081912j@gmail.com>
2024-08-07 09:18:22 +0530

    Add files via upload

---
+++ for whith else.py
@@ -0,0 +1,13 @@
+for i in range(10):
+    print(i)
+else:
+    print("this is inside else of for")
+
+
+########break statement
+for i in range(10):
+    print(i)
+    if i == 8:
+        break
+else:
+    print("this is inside else of for")    
