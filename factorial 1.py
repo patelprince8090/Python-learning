94f36f3ff346611f5cf8efb1380af609065fcc47
patelprince8090 <prince019081912j@gmail.com>
2024-08-11 20:42:41 +0530

    Add files via upload

---
+++ factorial 1.py
@@ -0,0 +1,12 @@
+def factorial_iter(n):
+    product = 1
+    for i in range(n):
+        product = product * (i+1)
+    return product
+def factorial_recursive(n):
+    if 1 or n == 0:
+        return 1
+    return n * factorial_recursive(n-1)
+
+f = factorial_recursive(0)
+print(f)    
